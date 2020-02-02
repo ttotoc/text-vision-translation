f210696298ec1383aa61a735fee229b32889a7ba
ttotoc <totoc.traian@gmail.com>
2020-02-02 18:29:13 +0200

    add the 'helper' package

---
+++ text-vision-translation/helpers/consts.py
@@ -0,0 +1,8 @@
+import os
+
+# Paths
+PATH_PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+PATH_SRC_ROOT = os.path.dirname(os.path.dirname(__file__))
+PATH_IMAGES = os.path.join(PATH_PROJECT_ROOT, 'images')
+PATH_MODELS = os.path.join(PATH_PROJECT_ROOT, 'models')
+PATH_CONFIG = os.path.join(PATH_PROJECT_ROOT, 'cfg')

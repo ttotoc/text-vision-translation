90ab23a4f1991821e4964904398ad57308895592
ttotoc <totoc.traian@gmail.com>
2020-02-10 23:47:18 +0200

    remove ExitMenu class, better check for function type for menus

--- text-vision-translation/menu/menu.py
+++ text-vision-translation/menu/menu.py
@@ -1,11 +1,14 @@
+from types import FunctionType
+
+
 class Menu:
     HELP_OPTION = "help"
     MIN_OPTION_IDX = 1
 
     def __init__(self, option_args, description=None):
-        '''
+        """
         :param option_args: a list of (text, handler_or_menu) tuples that define the options
-        '''
+        """
 
         self.description = description
 
@@ -14,15 +17,12 @@ class Menu:
                 raise TypeError(f'Expected {tuple}, got {type(opt)}')
             if not isinstance(opt[0], str):
                 raise TypeError(f'Expected {str}, got {type(opt[0])}')
-            if not callable(opt[1]) and not isinstance(opt[1], Menu) and not opt[1] is ExitMenu:
-                raise TypeError(f'Expected {callable}, {Menu} or {ExitMenu}, got {type(opt[1])}')
+            if not isinstance(opt[1], FunctionType) and not isinstance(opt[1], Menu) and not opt[1] is None:
+                raise TypeError(f'Expected {FunctionType}, {Menu} or {None}, got {type(opt[1])}')
 
         self.options = option_args
 
     def open(self):
-        '''
-        :param input_val: an index of the option list
-        '''
         self.help()
 
         while True:
@@ -48,7 +48,7 @@ class Menu:
             # execute option function, open menu or exit menu
             option_idx = input_val - 1
             option_action = self.options[option_idx][1]
-            if option_action is ExitMenu:
+            if option_action is None:
                 break
             elif isinstance(option_action, Menu):
                 option_action.open()
@@ -58,14 +58,9 @@ class Menu:
             self.help()
 
     def help(self):
-        '''
+        """
         prints the options and description of menu
-        '''
+        """
         print(self.description)
         for i, opt in enumerate(self.options):
             print(f'{i + 1}. {opt[0]}')
-
-
-# used when you want for an option to exit from the current menu
-class ExitMenu:
-    pass
